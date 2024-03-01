c5cb4bfc91e8bed41d3ef2280fc978fc62d27ae0
Emma Jessee <emma.jessee@stu.glasgow.kyschools.us>
2024-03-01 10:24:46 -0600

    hi

---
+++ script.js
@@ -0,0 +1,31 @@
+//1.
+for (i = 1; i <=7; i++){
+    console.log(i);
+}
+
+//2. 
+
+for (i= 5; i<= 25; i+=4){
+    console.log(i);
+}
+
+//3a. 
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"];
+
+//3b. 
+for (wizard of wizards) {
+    console.log(wizards);
+}
+
+//4a. 
+let harryPotterMovies = 0;
+console.log(harryPotterMovies);
+
+//4b. 
+while (harryPotterMovies < 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies ++;
+}
+
+//4c.
+console.log(harryPotterMovies);
